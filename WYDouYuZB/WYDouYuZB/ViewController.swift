87925c8438cd2bd19d634eb84763bfe9df3cf094
wangyun-hero <wangyun120411@163.com>
2020-03-22 16:12:53 +0800

    测试提交

--- WYDouYuZB/WYDouYuZB/ViewController.swift
+++ WYDouYuZB/WYDouYuZB/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("测试提交")
         // Do any additional setup after loading the view.
     }
 
